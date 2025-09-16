62365223c9e4bcf0478e97a3cf52e74518210ddc
Mudit <mudit.daga@webkorps.com>
2025-09-16 22:23:07 +0530

    implements spring security

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -65,6 +66,15 @@ public class HomeController {
 		User result = userRepository.save(user);
 		System.out.println("user" + result);
 		return "signup";
+	}
+	
+	
+	//handler for custom login
+	@GetMapping("/signin")
+	public String customLogin(Model model)
+	{
+		model.addAttribute("title", "Login Page");
+		return "login";
 	}
 
 }
